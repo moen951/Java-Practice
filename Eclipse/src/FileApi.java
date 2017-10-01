71f38cf00448a352e4c93a1111009829f05fca1c
moen951 <ahmadamirul2@gmail.com>
2017-10-01 19:23:17 +0800

    lab 3

---
+++ Eclipse/src/FileApi.java
@@ -0,0 +1,73 @@
+import java.io.*;
+class FileApi extends File
+{
+	String path = "E:/Window Document/github/Java-Practice/Eclipse/src/";
+	FileWriter fw;
+	FileReader fr;
+	
+	File file;
+	FileApi(String s)
+	{
+		super(s);
+		file = new File(s+".txt");
+	}
+
+	boolean fileExists()
+	{
+		if(file.exists()+ path != null)
+			return true;
+		else
+			return false;
+	}
+	/**
+	This method is used to create a new file in the 
+      current directory
+
+*/
+void createFile() throws IOException
+{
+		fw = new FileWriter(file);
+		fw.close();
+}
+
+/**
+	This method is used to write text into file
+	
+*/
+void writeFile(String s)
+{
+	try
+	{
+		fw = new FileWriter(path+file);
+		fw.write(s);
+		fw.close();
+		
+		
+	}catch(IOException asdf){
+		asdf.printStackTrace();
+	}
+}
+/**
+	This method is used to read text from file
+		
+*/
+String readFile() throws Exception
+{
+	fr= new FileReader(path+file);
+	BufferedReader br = new BufferedReader(fr);
+	String data;
+	while((data = br.readLine()) == null)
+	{
+		System.out.println(data);
+	
+	}
+	fr.close();
+	return data;
+	
+	/*
+	This method should be able to read the content of  existing text file and return all the text as string
+	*/		
+
+}
+}
+
---
+++ Eclipse/src/MyUtama.java
@@ -0,0 +1,31 @@
+import java.io.*;
+class MyUtama
+{
+	public static void main(String [] args) throws Exception
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("Input file name: ");
+		String namafail = br.readLine();
+		FileApi fileapi = new FileApi(namafail);
+
+		if(fileapi.fileExists())
+			{
+				System.out.println("File Exists!");
+				System.out.println(fileapi.readFile());
+			}
+		else
+			{
+				System.out.println("File does not exist!\n");
+			
+				
+				System.out.println(" "+namafail+ " has been created");
+
+				System.out.println("\n Insert text to be saved into file:");
+				String content = br.readLine();
+				fileapi.writeFile(content);
+				System.out.println("\n The text above is save into file "+namafail+".txt");
+		}
+	}
+
+}
+
